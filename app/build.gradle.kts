e72332921e8bdef7153fb83967f6dc5088819eb9
Roberto Diaz <rdiazmartin@gmail.com>
2026-08-09 23:18:51 +0200

    chore: bump version a 0.1.2 (versionCode 3) para release v0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.stayalert"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
